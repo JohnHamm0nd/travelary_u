1201370a98e0960090b07b35a713a41e4c602bf8
JohnHamm0nd <junho313@naver.com>
2019-12-11 11:34:42 +0900

    new

--- data/forms.py
+++ data/forms.py
@@ -13,7 +13,7 @@ class ReviewForm(forms.ModelForm):
         fields = ['content', 'rate']
         
 
-# model 에서 이미지 클래스를 보면 이미지 처리가 있는데 여기서도 해야 하나? -> 없어도 된다.
+# model 에서 이미지 클래스를 보면 이미지 처리가 있는데 여기서도 해야 하나? -> 없어도 되는듯? model 이 데이터형 정의만 하는게 아니라 처리까지 가능??
 # 둘 중에 하나만 해도 되는 것이면 어디서 하는게 더 좋은가?
 class ImageForm(forms.ModelForm):
 
--- data/models.py
+++ data/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
 from django.urls import reverse
+from django.db.models import F, Func # 리뷰점수, 저장 계산을 위한 라이브러리
 
 # Create your models here.Create
 class Data(models.Model):
@@ -12,9 +13,9 @@ class Data(models.Model):
     name = models.CharField(max_length=100) 
     businessCategory = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
-    desc = models.CharField(max_length=100)
-    microReview = models.CharField(max_length=100)
-    tags = models.CharField(max_length=100)
+    desc = models.CharField(max_length=100, null=True)
+    microReview = models.CharField(max_length=100, null=True)
+    tags = models.CharField(max_length=100, null=True)
     options = models.CharField(max_length=100)
     totalReviewCount = models.IntegerField()
     roadAddr = models.CharField(max_length=100)
@@ -23,7 +24,11 @@ class Data(models.Model):
     phone = models.CharField(max_length=100)
     x = models.FloatField()
     y = models.FloatField()
-    imageSrc = models.CharField(max_length=200)
+    imageSrc = models.CharField(max_length=300)
+    # 리뷰점수(리뷰점수합, 카운트, 리뷰평균점수)
+    sum_review_score = models.FloatField(default=0)
+    review_count = models.IntegerField(default=0)
+    average_review_score = models.FloatField(default=0)
 
 def post_image_path(instance, filename):
     return 'reviews/{}/{}'.format(instance.review.pk, filename)
@@ -38,8 +43,21 @@ class Review(models.Model):
     def get_absolute_url(self):
         return reverse('data:detail', kwargs={'pk': self.data_id})
     
+    # 리뷰 카운트, 리뷰점수, 평균점수 저장을 위해 form save 방법 변경. update 를 사용하여 Data 모델에 있는 필드 값 변경
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            Data.objects.filter(pk=self.data_id).update(review_count=F('review_count')+1)
+            Data.objects.filter(pk=self.data_id).update(sum_review_score=F('sum_review_score')+self.rate)
+            Data.objects.filter(pk=self.data_id).update(average_review_score=Round(F('sum_review_score')/F('review_count'), 1))
+        super().save(*args, **kwargs)
+    
     class Meta:
         ordering = ['-created_at']
+
+# Func 함수를 받아서 반올림 함수 만들어 사용
+class Round(Func):
+  function = 'ROUND'
+  # arity = 2
 
 class Image(models.Model):
     review = models.ForeignKey(Review, on_delete=models.CASCADE)
@@ -49,8 +67,7 @@ class Image(models.Model):
                             null = True,
                             processors=[ResizeToFill(716,537)],
                             format="JPEG",
-                            options={'quality':90},
+                            options={'quality':100},
                                     )
     
-       
-    
+
